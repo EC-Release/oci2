e1ae57d0f11072d48f77d071421de346a122b15f
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-24 12:42:04 -0800

    Create docker-deployment-sim.sh

---
+++ scripts/docker-deployment-sim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
+#docker run -rm --env-file=test/gateway.env enterpriseconnect/agent:v1 -ver
+printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
+docker run --rm --env-file=test/gateway.env enterpriseconnect/agent:v1 -ver
+printf "\n\n\n*** test lber w/ docker\n\n"
+docker run --rm enterpriseconnect/lb:v1
+
+printf "\n\n\n*** test server+tls v1 plugin w/ docker\n\n"
+#docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
+docker run --rm --name server-tls --env-file=test/server+tls.env enterpriseconnect/plugins:v1 -ver
+printf "\n\n\n*** test client+vln v1 plugin w/ docker\n\n"
+docker run --rm --name client-vln --env-file=test/client+vln.env enterpriseconnect/plugins:v1 -ver
