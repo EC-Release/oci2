0ccdda83eb56c75133d82f56e1a36f5dad5d43c3
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-24 19:56:50 -0800

    Update docker-deployment-sim.sh

--- scripts/docker-deployment-sim.sh
+++ scripts/docker-deployment-sim.sh
@@ -2,12 +2,12 @@
 #printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
 #docker run -rm --env-file=test/gateway.env enterpriseconnect/agent:v1 -ver
 printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
-docker run --rm --env-file=e2e/gateway.env enterpriseconnect/agent:v1 -ver
+docker run --rm --env-file=e2e/gateway.env enterpriseconnect/agent:v1beta -ver
 printf "\n\n\n*** test lber w/ docker\n\n"
-docker run --rm enterpriseconnect/lb:v1
+docker run --rm enterpriseconnect/lb:v1beta
 
 printf "\n\n\n*** test server+tls v1 plugin w/ docker\n\n"
 #docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
-docker run --rm --name server-tls --env-file=e2e/server+tls.env enterpriseconnect/plugins:v1 -ver
+docker run --rm --name server-tls --env-file=e2e/server+tls.env enterpriseconnect/plugins:v1beta -ver
 printf "\n\n\n*** test client+vln v1 plugin w/ docker\n\n"
-docker run --rm --name client-vln --env-file=e2e/client+vln.env enterpriseconnect/plugins:v1 -ver
+docker run --rm --name client-vln --env-file=e2e/client+vln.env enterpriseconnect/plugins:v1beta -ver
