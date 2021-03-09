9d5d684ffd65a89c2d0883925c2fb3d56800a1dd
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-09 13:54:07 -0800

    Update docker-deployment-sim.sh

--- scripts/docker-deployment-sim.sh
+++ scripts/docker-deployment-sim.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-#printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
-#docker run -rm --env-file=test/gateway.env enterpriseconnect/agent:v1 -ver
-printf "\n\n\n*** test v1 agt mode w/ docker\n\n"
-docker run -e AGENT_REV=${AGENT_REV} -e EC_PPS=${EC_PPS} --env-file=e2e/gateway.env enterpriseconnect/agent:v1 -ver
-printf "\n\n\n*** test lber w/ docker\n\n"
-docker run enterpriseconnect/lb:v1
-
-printf "\n\n\n*** test server+tls v1 plugin w/ docker\n\n"
-#docker run -it --rm -d --name server-tls --env-file=k8s/example/server+tls.env enterpriseconnect/plugins:v1 && sleep 5 && docker logs server-tls 
-docker run --rm --name server-tls --env-file=e2e/server+tls.env enterpriseconnect/plugins:v1 -ver
-printf "\n\n\n*** test client+vln v1 plugin w/ docker\n\n"
-docker run --rm --name client-vln --env-file=e2e/client+vln.env enterpriseconnect/plugins:v1 -ver
-printf "\n\n\n*** test lber w/ docker\n"
-docker run --rm --name client-vln --env-file=e2e/lber.env enterpriseconnect/lb:v1
+printf "\n\n\n*** test tengu api agt w/ docker\n\n"
+docker run enterpriseconnect/api:v1.2beta -ver
